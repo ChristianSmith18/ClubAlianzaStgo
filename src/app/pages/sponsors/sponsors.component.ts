8d3aa4b870e84b1cb5933de2d79680e19e7e7297
Christian Smith <cgr.gonzalezrossier@gmail.com>
2020-04-01 23:23:06 -0300

    Se subieron fotos, nombres y descipciones de los auspiciadores

--- src/app/pages/sponsors/sponsors.component.ts
+++ src/app/pages/sponsors/sponsors.component.ts
@@ -5,11 +5,35 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './sponsors.component.html',
   styleUrls: ['./sponsors.component.scss']
 })
-export class SponsorsComponent implements OnInit {
+export class SponsorsComponent {
+  sponsors = [
+    {
+      name: 'ACC Outsourcing Ltda',
+      description: 'Empresa que se dedica a los servicios para todo tipo de empresas, en diferentes áreas como lo son la construcción, asesorías informáticas y eventos.',
+      url: '../../../assets/img/sponsors/acc.jpeg'
+    },
+    {
+      name: 'Terminal Pesquero Metropolitano',
+      description: 'Lugar que alberga una variedad de productos del mar además de la distribución a toda la región metropolitana.',
+      url: '../../../assets/img/sponsors/terminal.jpeg'
+    },
+    {
+      name: 'Moro Sport',
+      description: 'Empresa chilena dedicada a la confección de ropa deportiva. Con 20 años de experiencia en el rubro textil.',
+      url: '../../../assets/img/sponsors/moro.jpeg'
+    },
+    {
+      name: 'Cerveza Muday',
+      description: 'Empresa chilena dedicada a la fabricación, preparación y distribución de cerveza artesanal en sus distintas variedades.',
+      url: '../../../assets/img/sponsors/muday.jpeg'
+    },
+    {
+      name: 'ETFA Ruido',
+      description: 'Empresa especializada en fiscalización ambiental de ruido.',
+      url: '../../../assets/img/sponsors/etfa.jpeg'
+    }
+  ];
 
   constructor() { }
-
-  ngOnInit() {
-  }
 
 }
